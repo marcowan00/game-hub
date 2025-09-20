0e4c27a0a8c405d0c7fe5c7a6f462eb7d6bc1831
marcowan00 <marcowan00@gmail.com>
2025-09-20 19:14:17 +0800

    Update page title

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -33,7 +33,11 @@ const PlatformIconList = ({ platforms }: Props) => {
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+        <Icon
+          key={platform.id}
+          as={iconMap[platform.slug] as React.ElementType}
+          color="gray.500"
+        />
       ))}
     </HStack>
   );
